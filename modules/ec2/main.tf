2bd37e34e3b0d597705d38e3b1e3557505b8da12
Bhaurao <bhauraomane@gmail.com>
2026-04-08 22:32:46 +0530

    updated ec2 main

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,3 +6,13 @@ module "ec2" {
     source = "./ec2"
     for_each = toset(["dev", "test", "prod"])
     }
+
+resource "aws_instance" "ec2" {
+  for_each = toset(["dev", "test", "prod"])
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${each.key}-instance"
+  }
+  
+}
